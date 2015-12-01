f43f396d36d46c7008342853f28f9bf42c0ceca3
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2015-12-01 12:13:25 -0500

    Benchmarks should match better

--- tests/test.c
+++ tests/test.c
@@ -12,12 +12,13 @@ void iferr(libtrace_t *trace)
 	exit(1);
 }
 
-int test(libtrace_t* trace) {
-	int psize = 0;
-	libtrace_packet_t* packet = trace_create_packet();
-	while ((psize = trace_read_packet(trace, packet)) > 0);
-	trace_destroy_packet(packet);
-	return psize;
+void test(libtrace_packet_t** packets, int len) {
+    int i = 0;
+    while (i < len) {
+        libtrace_tcp_t* tcp = trace_get_tcp(packets[i++]);
+        uint32_t rem;
+        void* data = trace_get_payload_from_tcp(tcp, &rem);
+    }
 }
 
 int main(char** args) {
@@ -28,21 +29,41 @@ int main(char** args) {
 	struct timespec start_time;
 	struct timespec end_time;
 	int exit_code = 0;
-	int i;
+	int i, psize = 0;
+
+    libtrace_packet_t **packets = malloc(100 * sizeof(libtrace_packet_t*));
+    i = 0;
+
+    libtrace_t* trace = trace_create("pcap:100_packets.pcap");
+    iferr(trace);
+    trace_start(trace);
+    libtrace_packet_t* packet = trace_create_packet();
+	while ((psize = trace_read_packet(trace, packet)) > 0){
+        libtrace_tcp_t* tcp = trace_get_tcp(packet);
+        if(tcp != NULL){
+            packets[i++] = packet;
+            packet = trace_create_packet();
+        } 
+
+    }
+    int len = i;
+
 	for (i = 0; i < 1000; i++) {
 		clock_gettime(CLOCK_MONOTONIC, &start_time);
-		libtrace_t* trace = trace_create("pcap:100_packets.pcap");
-		iferr(trace);
-		trace_start(trace);
-		exit_code = test(trace);
+        test(packets, len);
 		clock_gettime(CLOCK_MONOTONIC, &end_time);
 		total_ns += (end_time.tv_sec - start_time.tv_sec) * 1000000000 + (end_time.tv_nsec - start_time.tv_nsec);
-		if (exit_code != 0) break;
 	}
-	if (exit_code == 0) {
-		printf("Took %ld nanoseconds.\n", total_ns / 1000);
-	} else {
-		printf("Error! %d on iter %d\n", exit_code, i);
-	}
+
+    int runs = i;
+
+    for (i = 0; i < len; i++){
+        trace_destroy_packet(packets[i]);
+    }
+
+    free(packets);
+
+    printf("RUNS: %d, total_ns: %ld\n", runs, total_ns);
+    printf("Took %ld nanoseconds.\n", total_ns/runs);
 
 }
