31b1eed421a4d94485592dc9b4cddc279b802d62
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2015-12-01 14:41:44 -0500

    Fixed benches.rs and changed optimization level

--- benches/benches.rs
+++ benches/benches.rs
@@ -12,7 +12,7 @@ fn bench_pcap(b: &mut Bencher) {
     const IPV4_PACKET_TYPE : usize = 0x17;
     const TCP_PACKET : u8 = 6;
         
-    let mut cap = pcap::Capture::from_file("tests/100_packets.pcap").unwrap();
+    let mut cap = pcap::Capture::from_file("benches/100_packets.pcap").unwrap();
     let mut data = Vec::new();
     while let Ok(packet) = cap.next() {
         if packet[IPV4_PACKET_TYPE] as u8 == TCP_PACKET {
