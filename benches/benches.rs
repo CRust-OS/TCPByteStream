ead3e5ce900a81e4afa83ef8464a93cbb761f3e8
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2015-12-01 13:04:56 -0500

    Added updated benches

--- benches/benches.rs
+++ benches/benches.rs
@@ -11,19 +11,22 @@ fn bench_pcap(b: &mut Bencher) {
     const IPV4_START : u8 = 0x0E;
     const IPV4_PACKET_TYPE : usize = 0x17;
     const TCP_PACKET : u8 = 6;
-    
         
-    b.iter(|| {
-       	let mut cap = pcap::Capture::from_file("tests/100_packets.pcap").unwrap();
-        while let Ok(packet) = cap.next() {
-            // skip any non-tcp packets
-            if packet[IPV4_PACKET_TYPE] as u8 != TCP_PACKET {
-                continue;
-            }
-            // get start of TCP header
+    let mut cap = pcap::Capture::from_file("tests/100_packets.pcap").unwrap();
+    let mut data = Vec::new();
+    while let Ok(packet) = cap.next() {
+        if packet[IPV4_PACKET_TYPE] as u8 == TCP_PACKET {
             let tcp_len = 4 * (packet[0x0E] & 0x0F);
             let start = IPV4_START + tcp_len; 
-            TcpSegment::parse(&packet[start as usize ..]);
+            data.push(packet[start as usize ..].into_iter().cloned().collect::<Vec<u8>>());
+        }
+    }
+
+    b.iter(|| {
+        {
+            for tcp in data.iter() {
+                TcpSegment::parse(&tcp);
+            }
         }
    }); 
 }
